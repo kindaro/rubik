b7393b68f9cac4f5065999379ee33fa13a0a047e
Ignat Insarov <kindaro@gmail.com>
2022-09-27 18:45:47 +0200

    Symmetries work.

--- library/Data/Group/Symmetry.hs
+++ library/Data/Group/Symmetry.hs
@@ -1,25 +1,74 @@
+{-# LANGUAGE UndecidableInstances #-}
+
 module Data.Group.Symmetry where
 
 import Prelude.Unicode
 
+import Data.Type.Equality
 import GHC.TypeLits
 
 import Data.Finite
 import Data.Finite.Simplex
+import Data.Function
 import Data.Group.Permutation
 
--- | This fancy type is isomorphous to the functions `Finite n → Finite n` that are invertible.
-data Symmetry (n ∷ Natural) where
-  SuccessorSymmetry ∷ KnownNat n ⇒ Symmetry n → Finite (n + 1) → Symmetry (n + 1)
-  ZeroSymmetry ∷ Symmetry 0
+-- | This fancy type is isomorphous to the functions `Finite size → Finite size` that are invertible.
+data Symmetry (size ∷ Natural) where
+  SuccessorSymmetry ∷ (KnownNat size, size ~ ((size + 1) - 1)) ⇒ Symmetry size → Finite (size + 1) → Symmetry (size + 1)
+  ZeroSymmetry ∷ Symmetry 1
 
-symmetryToPermutation ∷ ∀ n. KnownNat n ⇒ Symmetry n → Permutation (Finite n)
+infixr 8 :×
+pattern (:×) ∷ (KnownNat size, size ~ ((size + 1) - 1)) ⇒ Finite (size + 1) → Symmetry size → Symmetry (size + 1)
+pattern finite :× symmetry = SuccessorSymmetry symmetry finite
+
+pattern Z ∷ Symmetry 1
+pattern Z = ZeroSymmetry
+
+class Equality α β where
+  equality ∷ α → β → Bool
+
+instance (KnownNat size₁, KnownNat size₂) ⇒ Equality (Finite size₁) (Finite size₂) where
+  equality finite₁ finite₂ = getFinite finite₁ ≡ getFinite finite₂ ∧ natVal finite₁ ≡ natVal finite₂
+
+instance (KnownNat size₁, KnownNat size₂) ⇒ Equality (Symmetry size₁) (Symmetry size₂) where
+  equality ZeroSymmetry ZeroSymmetry = True
+  equality (SuccessorSymmetry symmetry destination) (SuccessorSymmetry symmetry' destination') =
+    equality symmetry symmetry' ∧ equality destination destination'
+  equality _ _ = False
+
+instance KnownNat size ⇒ Eq (Symmetry size) where
+  symmetry₁ == symmetry₂ = equality symmetry₁ symmetry₂
+
+instance Show (Symmetry size) where
+  show ZeroSymmetry = "Z"
+  show (SuccessorSymmetry symmetry destination) = (show ∘ getFinite) destination ++ ":×" ++ show symmetry
+
+symmetryToPermutation ∷ ∀ size. KnownNat size ⇒ Symmetry size → Permutation (Finite size)
 symmetryToPermutation ZeroSymmetry = mempty
-symmetryToPermutation (SuccessorSymmetry previousSymmetry whereLastOneGoes) =
+symmetryToPermutation (SuccessorSymmetry previousSymmetry destination) =
   let Permutation{..} = symmetryToPermutation previousSymmetry
    in Permutation
-        { from = \finite → case strengthen finite of
-            Nothing → whereLastOneGoes
-            Just smallerFinite → (face whereLastOneGoes ∘ from) smallerFinite
-        , to = \finite → if finite ≡ whereLastOneGoes then maxBound else (weaken ∘ to ∘ degeneracy whereLastOneGoes) finite
+        { to = \source → case strengthen source of
+            Nothing → destination
+            Just strongerSource → (face destination ∘ to) strongerSource
+        , from = \source →
+            if source ≡ destination
+              then maxBound
+              else (weaken ∘ from ∘ degeneracy destination) source
         }
+
+class PermutationToSymmetry (size ∷ Natural) where
+  permutationToSymmetry ∷ KnownNat size ⇒ Permutation (Finite size) → Symmetry size
+
+instance {-# OVERLAPPING #-} PermutationToSymmetry 1 where
+  permutationToSymmetry _ = ZeroSymmetry
+
+instance (KnownNat n, KnownNat m, m ~ n + 1, n ~ m - 1, PermutationToSymmetry n) ⇒ PermutationToSymmetry m where
+  permutationToSymmetry Permutation{..} =
+    let recurse =
+          permutationToSymmetry @n
+            Permutation
+              { to = degeneracy (to maxBound) ∘ to ∘ face maxBound
+              , from = degeneracy (from maxBound) ∘ from ∘ face maxBound
+              }
+     in SuccessorSymmetry recurse (to maxBound)
